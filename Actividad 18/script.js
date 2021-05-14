28ffc48cd0041c937da9fd92576097473ddd130f
Axel Dali <a329881@uach.mx>
2021-05-14 11:24:24 -0600

    Actividad 18

---
+++ Actividad 18/script.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            usuarios: []
+        }
+    },
+    created(){
+        this.obtenerUsuarios()
+    },
+    methods: {
+        obtenerUsuarios(){
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response => response.json())
+                .then(users => {
+                    this.usuarios = users
+                })
+        }
+    },
+})
+
+const vm = app.mount('#app')
